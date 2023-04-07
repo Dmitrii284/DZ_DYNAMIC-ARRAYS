e6e18d2a4954f883bb7fd73e9cbe13abf621953e
Дмитрий <@ADMIN>
2023-04-07 22:59:55 +0500

    Добавьте файлы проекта.

---
+++ DYNAMIC ARRAYS/arr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>  // arr[i] = rand() % (10 + 1 - 1) + 1; // [1 ...10] Случайные числа  массива
+#include <ctime> //srand(time(NULL)); Случайные числа  массива
+#include <algorithm> // функция  stor  Быстрая сортировка std::sort(arr, arr + size);  
+// функцию swap Сортировка через сравнениемассивов std::swap(arr[j], arr[j + 1]);
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+
+
+
+	return 0;
+}
